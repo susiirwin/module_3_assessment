84116815b1f894a32fcb3b54d44651957c8695bb
SusiIrwin <susi.irwin@gmail.com>
2016-11-02 11:18:52 -0600

    Expect tests on feature

--- spec/features/user_can_see_stores_based_on_zip_spec.rb
+++ spec/features/user_can_see_stores_based_on_zip_spec.rb
@@ -18,11 +18,12 @@ RSpec.feature 'Search for stores with Zip' do
       # And I should see a message that says "17 Total Stores"
       expect(page).to have_content("17 Total Stores")
       # And I should see exactly 15 results
-      # expect(response.count).to eq(15)
-      # it "should have 15 li elements" do
-      #    expect(find('ul.text')).to have_selector('li', count: 15)
-      # end
+      expect(page).to have_selector('.fullname', count: 15)
       # And I should see the long name, city, distance, phone number and store type for each of the 15 results (so this is an iteration over the array in a bulleted list)
+      within first ".store" do
+        find('.fullname').text.should match("Best Buy Mobile - Cherry Creek Shopping Center")
+      end
+
       expect(page).to have_content("Name:")
       expect(page).to have_content("City:")
       expect(page).to have_content("Distance:")
