fcff63737e83410cf591c85870d9244c51e002f0
SusiIrwin <susi.irwin@gmail.com>
2016-11-02 10:56:17 -0600

    Store Poro

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,9 @@ class SearchController < ApplicationController
     result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=15&apiKey=uf3hrj9pa5u2jxvqa3xfq5a5")
     raw_results = JSON.parse(result.body, symbolize_names: true)[:stores]
 
-    raw_results.map do |store|
-      require 'pry'; binding.pry
+    @results = raw_results.map do |store|
+      Store.new(store)
     end
   end
+
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,17 @@
 class Store
+  attr_reader :long_name,
+              :city,
+              :distance,
+              :phone_number,
+              :store_type
 
   def initialize(store)
     require 'pry'; binding.pry
+    # @long_name = store[:]
+    # @city =
+    # @distance =
+    # @phone_number =
+    # @store_type =
   end
 
 end
