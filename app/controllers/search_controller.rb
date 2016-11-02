4091117e64d0d517c96202d1e51369e192082071
SusiIrwin <susi.irwin@gmail.com>
2016-11-02 10:49:02 -0600

    Iterating over raw_results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,9 @@ class SearchController < ApplicationController
   def index
     result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=15&apiKey=uf3hrj9pa5u2jxvqa3xfq5a5")
     raw_results = JSON.parse(result.body, symbolize_names: true)[:stores]
-    require 'pry'; binding.pry
 
     raw_results.map do |store|
-      Store.new()
+      require 'pry'; binding.pry
     end
   end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,7 @@
 class Store
-  
+
+  def initialize(store)
+    require 'pry'; binding.pry
+  end
 
 end
