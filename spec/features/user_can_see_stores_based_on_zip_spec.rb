0f8025c14ea6270d5c2bed4b3c3e9a45c2c97451
SusiIrwin <susi.irwin@gmail.com>
2016-11-02 11:10:09 -0600

    Basic structure of Search Page is built

--- spec/features/user_can_see_stores_based_on_zip_spec.rb
+++ spec/features/user_can_see_stores_based_on_zip_spec.rb
@@ -19,6 +19,9 @@ RSpec.feature 'Search for stores with Zip' do
       expect(page).to have_content("17 Total Stores")
       # And I should see exactly 15 results
       # expect(response.count).to eq(15)
+      # it "should have 15 li elements" do
+      #    expect(find('ul.text')).to have_selector('li', count: 15)
+      # end
       # And I should see the long name, city, distance, phone number and store type for each of the 15 results (so this is an iteration over the array in a bulleted list)
       expect(page).to have_content("Name:")
       expect(page).to have_content("City:")
